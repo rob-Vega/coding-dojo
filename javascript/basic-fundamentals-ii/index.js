287c91330cd4c16c08a143642ce868272505497f
Roberto Vega <rvegag95@gmail.com>
2023-04-26 21:54:29 -0400

    basic-fundamentals-ii completed and .gitignore modified

--- javascript/basic-fundamentals-ii/index.js
+++ javascript/basic-fundamentals-ii/index.js
@@ -86,3 +86,61 @@ const incrementSeconds = (array = []) => {
 };
 
 console.log(incrementSeconds([1, 2, 3, 4, 5]));
+
+const previousLengths = (array = []) => {
+  return array.map((value) => value.length);
+};
+
+console.log(previousLengths(["Roberto", "Paula", "Wilson"]));
+
+const addSeven = (array = []) => {
+  const newArray = array.map((value) => value + 7);
+  console.log("array original", array);
+  return newArray;
+};
+
+console.log(addSeven([1, 2, 3, 4, 5]));
+
+const inverseArray = (array = []) => {
+  const newArray = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    newArray.push(array[i]);
+  }
+  return newArray;
+};
+
+console.log(inverseArray([1, 2, 3, 4, 5, 6, 7]));
+
+const alwaysHungry = (array = []) => {
+  if (!array.includes("comida")) {
+    console.log("Tengo hambre :(");
+    return;
+  }
+  array.forEach((value) => {
+    if (value.toLowerCase() === "comida") {
+      console.log("Yummy ;P");
+    }
+  });
+};
+
+alwaysHungry(["comida", "comida", "no hay comida"]);
+alwaysHungry(["no hay comida", "no hay comida"]);
+
+const changeToTheCenter = (array = []) => {
+  let temp;
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    temp = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = temp;
+  }
+  console.log(array);
+};
+
+changeToTheCenter([1, 2, 3, 4, 5, 7, 8]);
+
+const scaleTheArray = (array = [], num = 0) => {
+  if (array.length === 0) return;
+  return array.map((value) => value * num);
+};
+
+console.log(scaleTheArray([1, 2, 3, 4, 5, 6, 7], 4));
